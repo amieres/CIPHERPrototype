cbb39ac0e8709dbd2b07f8cfbae522c80700ba54
amieres <amieres@hotmail.com>
2017-08-03 06:47:13 -0500

    "Fixed" FSharp.Core.dll issue

---
+++ WebServer/FSharpStation - Copy.cmd
@@ -0,0 +1,3 @@
+PATH=D:\Abe\CIPHERWorkspace\CIPHERPrototype\WebServer\bin\Compiled;C:\Program Files (x86)\Microsoft SDKs\F#\4.1\Framework\v4.0;%PATH%
+cd bin
+start cmd.exe @cmd /k FSAutoComplete.exe
