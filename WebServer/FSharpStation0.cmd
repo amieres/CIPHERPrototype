782708f0ae87c1d052bb9bc7553292da88104398
amieres <amieres@hotmail.com>
2017-09-10 07:02:57 -0500

    FSharpStation independent

--- WebServer/FSharpStation0.cmd
+++ WebServer/FSharpStation0.cmd
@@ -1,4 +1,4 @@
 start /max http:\\localhost:9010
 PATH=%PATH%;C:\Program Files (x86)\Microsoft SDKs\F#\4.1\Framework\v4.0
 cd bin
-start cmd.exe @cmd /k Compiled\FSharpStation\FSharpStation.exe
+start cmd.exe @cmd /k Compiled\FSharpStation\FSharpStation0.exe
---
+++ WebServer/WebServer.cmd
@@ -0,0 +1,4 @@
+start /max http:\\localhost:9000\FSharpStation.html
+PATH=%PATH%;C:\Program Files (x86)\Microsoft SDKs\F#\4.1\Framework\v4.0
+cd bin
+start cmd.exe @cmd /k WebServer.exe
