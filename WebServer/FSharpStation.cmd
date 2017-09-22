5324cf74258310769a4163d8545e0b9fb48cd758
amieres <amieres@hotmail.com>
2017-09-22 08:50:12 -0500

    WS Beta10

--- WebServer/FSharpStation.cmd
+++ WebServer/FSharpStation.cmd
@@ -1,4 +1,4 @@
 start /max http:\\localhost:9010
-PATH=%PATH%;C:\Program Files (x86)\Microsoft SDKs\F#\4.1\Framework\v4.0
+PATH=%PATH%;C:\Program Files (x86)\Microsoft SDKs\F#\4.1\Framework\v4.0;..\..\Common\packages\Zafir.FSharp\tools
 cd bin
-start cmd.exe @cmd /k Compiled\FSharpStation\FSharpStation.exe
+start cmd.exe @cmd /k Compiled\FSharpStation\FSharpStation.exe "website" "http://localhost:9010/"
